5e82d09a7bf969391fcf21c68e65709859251e3a
Anurag Kumar <anuragak@users.noreply.github.com>
2018-06-12 23:57:47 +0100

    added responsive sizing features

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
 const projects = [
   {title: "Random Quote Generator",
-  description: "Randomly display a quote from an array and also change the background colour.",
+  description: "Displays a quote from an array of 10 quotes and changes the background colour every 30 seconds or on the users click.",
   link: "https://arvindak.github.io/THFS-Project1_QuotesDisplay/#",
-  img: "media/project1.png",
-  buttonTxt: "Project 1",
-  cssClass: "small"
+  projectNumber: 1
   },
+
   {title: "Pagination and dynamic filtering",
   description: "Dynamicly search and filtering a dataset of students.",
   link: "https://arvindak.github.io/THFS-Project2_Pagination/#",
-  img: "media/project2.png",
-  buttonTxt: "Project 2",
-  cssClass: "large"
+  projectNumber: 2
   },
-  {title: "Pagination and dynamic filtering",
+
+  {title: "Form validation",
   description: "Using client side validation that provides real timeout feedback to the user.",
   link: "https://arvindak.github.io/THFS-Project3_InteractiveForm/#",
-  img: "media/project3.png",
-  buttonTxt: "Project 3",
-  cssClass: "tall"
+  projectNumber: 3
   },
+
   {title: "Tic-Tac-Toe",
   description: "Play against a local player or your computer.",
   link: "https://arvindak.github.io/THFS-Project4_Tic-Tac-Toe/#",
-  img: "media/project4.png",
-  buttonTxt: "Project 4",
-  cssClass: "medium"
+  projectNumber: 4
   },
+
   {title: "API Awesome Employee Fetching",
   description: "Fetch 12 users from an API, diplay them in cards and use modals to inspect each card.",
   link: "https://arvindak.github.io/THFS-Project5_API/#",
-  img: "media/project5.png",
-  buttonTxt: "Project 5",
-  cssClass: "large"
+  projectNumber: 5
   }
 ];
 
+
 (function(){
   let projectHTML = [];
-  projects.forEach(project =>{
-    someHTML = `
-      <div class="item"><img class="project-img" src=${project.img}></div>`
-     projectHTML += someHTML;
-  });
-
-document.querySelectorAll('.bg').forEach(card =>
-  card.addEventListener('mouseover', e=>{
-    console.log(e.target)
-  }));
+  projects.reverse().forEach(project =>{
+    let someHtml = `
+      <div class="flip3D item${project.projectNumber}">
+        <div class="back">
+          <h2 class="project-title">${project.title}</h2>
+          <p class="sub-heading">Treehouse project ${project.projectNumber}</p>
+          <p>${project.description}</p>
+          <a href=${project.link} class="button">Visit Project ${project.projectNumber}</a>
+        </div>
+        <div class="front">
+        </div>
+      </div>`;
+    projectHTML += someHtml;
+  })
+  document.querySelector('.wrapper').insertAdjacentHTML('afterBegin', projectHTML);
+})()
